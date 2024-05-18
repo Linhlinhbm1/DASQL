4921567828cd5dd36520263ddcef665ad4ce1c92
Linhlinhbm1 <153344152+Linhlinhbm1@users.noreply.github.com>
2024-05-18 15:46:29 +0700

    Update bailuyentap5.sql

--- bailuyentap5.sql
+++ bailuyentap5.sql
@@ -6,7 +6,13 @@ inner join country as b
 on a.countrycode=b.code
 group by b.continent
 --baitap2--
-
+SELECT ROUND(sum(CASE 
+    WHEN b.signup_action = 'Confirmed'
+     THEN 1
+    ELSE 0
+    END)::DECIMAL / count(*), 2)
+FROM emails AS a
+INNER JOIN texts AS b ON a.email_id = b.email_id
 --baitap3--
 SELECT 
     b.age_bucket,
@@ -26,6 +32,8 @@ group by a.customer_id
 having count (DISTINCT b.product_category) =3
 
 --baitap5--
+    
+--baitap6--
 select  a.product_name,
 sum(unit) as unit                        
 from Products as a
@@ -34,5 +42,13 @@ on a.product_id = b.product_id
 where month(b.order_date )=2
 group by a.product_name
 having sum(unit) >=100 
+    
+--baitap7--
+SELECT a.page_id
+FROM pages as a
+left join page_likes as b
+on a.page_id=b.page_id
+WHERE b.liked_date is null
+ORDER BY a.page_id
 
 
