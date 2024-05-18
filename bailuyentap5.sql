d4050a29bcb73488394e96cff4066029d723e3c2
Linhlinhbm1 <153344152+Linhlinhbm1@users.noreply.github.com>
2024-05-18 08:24:38 +0700

    Update bailuyentap5.sql

--- bailuyentap5.sql
+++ bailuyentap5.sql
@@ -6,3 +6,22 @@ inner join country as b
 on a.countrycode=b.code
 group by b.continent
 --baitap2--
+
+--baitap3--
+SELECT 
+    b.age_bucket,
+    round( 100* sum(a.time_spent) filter (WHERE a.activity_type='send')/sum(a.time_spent),2) AS send_perc,
+    round( 100* sum(a.time_spent) filter (WHERE a.activity_type='open')/sum(a.time_spent),2) AS open_perc
+FROM activities as a
+inner join age_breakdown as b 
+on a.user_id=b.user_id
+where a.activity_type in ('open','send')
+GROUP BY b.age_bucket
+--baitap4--
+SELECT a.customer_id 
+FROM customer_contracts as a  
+left join products as b 
+on a.product_id=b.product_id
+group by a.customer_id
+having count (DISTINCT b.product_category) =3
+
